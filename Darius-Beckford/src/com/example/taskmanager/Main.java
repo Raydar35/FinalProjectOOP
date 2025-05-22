8ae0b0e6639355bf5dc834ffe01a76fa4a88943d
Darius Beckford <d.beckford28@ncf.edu>
2025-05-21 22:27:52 -0400

    Initialize

--- Darius-Beckford/src/com/example/taskmanager/Main.java
+++ Darius-Beckford/src/com/example/taskmanager/Main.java
@@ -1,5 +1,7 @@
+package com.example.taskmanager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        TaskApp.main(args);
     }
 }
